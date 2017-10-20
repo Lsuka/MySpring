8a6cc242a07473e42a92840d81ad9d0f095b9838
Lsuka <1150859699@qq.com>
2017-10-21 00:12:00 +0800

    集合注入(Map的注入)

--- myspring-di/src/main/java/cn/unicorn/myspring/di/vo/Dept.java
+++ myspring-di/src/main/java/cn/unicorn/myspring/di/vo/Dept.java
@@ -1,49 +1,60 @@
 package cn.unicorn.myspring.di.vo;
 
 import java.io.Serializable;
-import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 public class Dept implements Serializable {
 	private Long deptno;
 	private String dname;
 	private Boolean enable;
 	// 追加对象数据组(观察数组注入使用)
-	private Set<Emp> emps;
-	private List<String> infos;
+	private Map<String, Emp> emps;
+	private Map<Integer, String> infos;
+
 	public Long getDeptno() {
 		return deptno;
 	}
+
 	public void setDeptno(Long deptno) {
 		this.deptno = deptno;
 	}
+
 	public String getDname() {
 		return dname;
 	}
+
 	public void setDname(String dname) {
 		this.dname = dname;
 	}
+
 	public Boolean getEnable() {
 		return enable;
 	}
+
 	public void setEnable(Boolean enable) {
 		this.enable = enable;
 	}
-	public Set<Emp> getEmps() {
+
+	public Map<String, Emp> getEmps() {
 		return emps;
 	}
-	public void setEmps(Set<Emp> emps) {
+
+	public void setEmps(Map<String, Emp> emps) {
 		this.emps = emps;
 	}
-	public List<String> getInfos() {
+
+	public Map<Integer, String> getInfos() {
 		return infos;
 	}
-	public void setInfos(List<String> infos) {
+
+	public void setInfos(Map<Integer, String> infos) {
 		this.infos = infos;
 	}
+
 	@Override
 	public String toString() {
 		return "Dept [deptno=" + deptno + ", dname=" + dname + ", enable=" + enable + ", emps=" + emps + ", infos="
 				+ infos + "]";
 	}
+
 }
