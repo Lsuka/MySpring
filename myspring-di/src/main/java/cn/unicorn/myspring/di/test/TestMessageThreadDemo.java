9a592f03a8647b94aeb6a85862b4e158cc43a4f0
Lsuka <1150859699@qq.com>
2017-10-21 15:05:38 +0800

    Bean的初始化和销毁处理

--- myspring-di/src/main/java/cn/unicorn/myspring/di/test/TestMessageThreadDemo.java
+++ myspring-di/src/main/java/cn/unicorn/myspring/di/test/TestMessageThreadDemo.java
@@ -1,20 +1,18 @@
 package cn.unicorn.myspring.di.test;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import cn.unicorn.myspring.di.vo.Dept;
 import cn.unicorn.myspring.di.vo.Message;
 
 public class TestMessageThreadDemo {
 	public static void main(String[] args) {
-		//观察Bean什么时候被实例化
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/spring-di.xml");
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring/spring-di.xml");
 		for (int i = 0; i < 3; i++) {
 			new Thread(() -> {
 				Message msg = ctx.getBean("message", Message.class);// 获取程序类
 				System.out.println(Thread.currentThread().getName() + ",msg = " + msg);
 			}).start();
 		}
+		ctx.registerShutdownHook();// 触发销毁方法
 	}
 }
