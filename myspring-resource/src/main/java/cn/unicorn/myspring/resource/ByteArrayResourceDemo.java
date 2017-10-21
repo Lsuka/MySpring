c2e7bdefeb8b649d3541c15b2daadb214816f5bd
Lsuka <1150859699@qq.com>
2017-10-21 20:43:42 +0800

    利用ResourceLoader接口解Resource与接口对象的耦合问题(实际上了可以发现Resource定义了资源的标准,而ResourceLoader定义了字符串与资源类型的关系)

--- myspring-resource/src/main/java/cn/unicorn/myspring/resource/ByteArrayResourceDemo.java
+++ myspring-resource/src/main/java/cn/unicorn/myspring/resource/ByteArrayResourceDemo.java
@@ -2,16 +2,17 @@ package cn.unicorn.myspring.resource;
 
 import java.util.Scanner;
 
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.ResourceLoader;
 
 public class ByteArrayResourceDemo {
 	public static void main(String[] args) throws Exception {
+		ResourceLoader resourceLoader = new DefaultResourceLoader();
 		// 创建一个内存去读的Resource对象
-		//Resource resource = new FileSystemResource(new File("D:"+File.separatorChar+"233.xml"));//本地文件
-		//Resource resource = new ClassPathResource("spring/spring-resource.xml");//ClassPath
-		//Resource resource = new ByteArrayResource("deep♂dark♂fanstansy".getBytes());//内存流
-		Resource resource = new UrlResource("http://www.baidu.com");
+		//Resource resource = resourceLoader.getResource("file:D:/233.xml");//本地文件
+		//Resource resource = resourceLoader.getResource("classpath:spring/spring-resource.xml");//ClassPath
+		Resource resource =resourceLoader.getResource("http://www.baidu.com");
 		// 读取内容可以使用Resource父接口InputStreamSrouce提供的getInputStream()方法来完成
 		Scanner scanner = new Scanner(resource.getInputStream());
 		while (scanner.hasNext()) {
