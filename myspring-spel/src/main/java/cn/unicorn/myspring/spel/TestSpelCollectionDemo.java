648836c3b0e8a20cf7dcb2152aa95e8a6ffd2078
Lsuka <1150859699@qq.com>
2017-10-22 15:57:31 +0800

    集合内容的修改

--- myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelCollectionDemo.java
+++ myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelCollectionDemo.java
@@ -1,7 +1,7 @@
 package cn.unicorn.myspring.spel;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
@@ -11,24 +11,16 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 public class TestSpelCollectionDemo {
 	public static void main(String[] args) throws Exception {
-		// 集合表达式
-		// List<String> all = new ArrayList<>();
-		// Set<String> all = new HashSet<>();
-		// all.add("van游戏");
-		// all.add("deep♂dark");
-		// all.add("ass♂we♂can");
-		Map<String, String> all = new HashMap<>();
-		all.put("van", "deep♂dark");
-		all.put("king", "ass♂we♂can");
-		String str = "#allData['king']";// 通过索引访问数据
+		//集合内容的修改
+		List<String> all = new ArrayList<>();
+		all.add("van游戏");
+		all.add("deep♂dark");
+		all.add("ass♂we♂can");
+		String str = "#allData[1]='www.bilibili.com'";//修改集合中的内容
 		ExpressionParser parser = new SpelExpressionParser();
 		Expression exp = parser.parseExpression(str);// 设置了一个自定义的根变量
 		EvaluationContext context = new StandardEvaluationContext();
 		context.setVariable("allData", all);// 将自定义的集合设置进来
 		System.out.println(exp.getValue(context));
-		// 尝试List数据的添加
-		// List<Integer> all = exp.getValue(context, List.class);
-		// all.add(50);//进行新的数据追加。不可以进行数据添加,报java.util.Collections$UnmodifiableCollection.add(Unknown
-		// Source)错误
 	}
 }
