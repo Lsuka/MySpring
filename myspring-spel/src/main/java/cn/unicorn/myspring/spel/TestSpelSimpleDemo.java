3caea83a41dc4b85fe162db42fd59a43b462435c
Lsuka <1150859699@qq.com>
2017-10-22 15:17:03 +0800

    SPEL的方法引用操作

--- myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelSimpleDemo.java
+++ myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelSimpleDemo.java
@@ -1,19 +1,22 @@
 package cn.unicorn.myspring.spel;
 
-import org.springframework.expression.EvaluationContext;
+import java.lang.reflect.Method;
+
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 public class TestSpelSimpleDemo {
-	public static void main(String[] args) {
-		//变量操作(根变量的加入)
-		String str = "#root=='mldn' ? 'bilibili' :'van游戏'";
-		ExpressionParser parser = new SpelExpressionParser();// 定义一个SPEL的语法解析器
-		Expression exp = parser.parseExpression(str);// 将字符串解析为一个正常的表达式对象
-		//为这个根变量直接执行赋值处理操作
-		EvaluationContext context = new StandardEvaluationContext("2333");// 实现表达式的具体计算处理
+	public static void main(String[] args) throws Exception {
+		// 变量操作(根变量的加入)
+		String str = "#myInt('666')";// myInt表示的就是方法引用设置的别名
+		Method method = Integer.class.getMethod("parseInt", String.class);// 获取该方法引用的反射对象
+		ExpressionParser parser = new SpelExpressionParser();
+		Expression exp = parser.parseExpression(str);// 设置了一个自定义的根变量
+		// 为这个根变量直接执行赋值处理操作
+		StandardEvaluationContext context = new StandardEvaluationContext();
+		context.registerFunction("myInt", method);
 		System.out.println(exp.getValue(context));
 	}
 }
