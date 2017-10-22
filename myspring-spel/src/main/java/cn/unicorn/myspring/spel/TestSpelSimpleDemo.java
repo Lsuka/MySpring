07fc3651f555c3c9da31fddf00d4b7c2649b2458
Lsuka <1150859699@qq.com>
2017-10-22 15:23:30 +0800

    // null处理,使用Groovy表达式处理

--- myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelSimpleDemo.java
+++ myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelSimpleDemo.java
@@ -1,7 +1,5 @@
 package cn.unicorn.myspring.spel;
 
-import java.util.Date;
-
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
@@ -10,13 +8,12 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 public class TestSpelSimpleDemo {
 	public static void main(String[] args) throws Exception {
-		// SPEL的方法引用操作(调用对象中的属性(getter))
-		String str = "Time";// Date类中的getTime()方法
+		// null处理,使用Groovy表达式处理
+		String str = "#root?.time";
 		ExpressionParser parser = new SpelExpressionParser();
 		Expression exp = parser.parseExpression(str);// 设置了一个自定义的根变量
 		// 为这个根变量直接执行赋值处理操作
-		EvaluationContext context = new StandardEvaluationContext(new Date());
+		EvaluationContext context = new StandardEvaluationContext();
 		System.out.println(exp.getValue(context));
-		System.out.println(System.currentTimeMillis());
 	}
 }
