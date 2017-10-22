1f88125a097cb2ddd44a75468bc02d32ba39b75f
Lsuka <1150859699@qq.com>
2017-10-22 15:10:19 +0800

    变量的操作(根变量的引入)

--- myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelSimpleDemo.java
+++ myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelSimpleDemo.java
@@ -8,23 +8,12 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 public class TestSpelSimpleDemo {
 	public static void main(String[] args) {
-		//Class类型表达式
-		//获取Class
-//		String str = "T(java.lang.String)";
-//		String str = "T(java.util.Date)";
-		//静态属性
-//		String str = "T(Integer).MAX_VALUE";
-//		String str = "T(Integer).parseInt('6666')";
-		//对象实例化
-//		String str = "new java.util.Date()";
-		//instanceof
-		String str = "'van游戏' instanceof T(String)";
+		//变量操作(根变量的加入)
+		String str = "#root=='mldn' ? 'bilibili' :'van游戏'";
 		ExpressionParser parser = new SpelExpressionParser();// 定义一个SPEL的语法解析器
 		Expression exp = parser.parseExpression(str);// 将字符串解析为一个正常的表达式对象
-		EvaluationContext context = new StandardEvaluationContext(exp);// 实现表达式的具体计算处理
-//		Class<?> cls = exp.getValue(context,Class.class);
-//		Integer num = exp.getValue(context,Integer.class);
-//		Date date = exp.getValue(context,Date.class);
+		//为这个根变量直接执行赋值处理操作
+		EvaluationContext context = new StandardEvaluationContext("2333");// 实现表达式的具体计算处理
 		System.out.println(exp.getValue(context));
 	}
 }
