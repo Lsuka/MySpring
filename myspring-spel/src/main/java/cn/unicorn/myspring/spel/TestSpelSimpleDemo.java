7d34b1f0b0280aff70791361618a71e1a73d6dee
Lsuka <1150859699@qq.com>
2017-10-22 14:49:25 +0800

    Class类型表达式

--- myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelSimpleDemo.java
+++ myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelSimpleDemo.java
@@ -3,19 +3,28 @@ package cn.unicorn.myspring.spel;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.ParserContext;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 public class TestSpelSimpleDemo {
 	public static void main(String[] args) {
-		//String str = "'helloworld'[1]";// 获取指定字符串,相当于charAt()
-		//String str = "'helloworld'.toUpperCase()";// 转大写
-		String str = "'helloworld'.replaceAll('l','2')";// 字符串替换
+		//Class类型表达式
+		//获取Class
+//		String str = "T(java.lang.String)";
+//		String str = "T(java.util.Date)";
+		//静态属性
+//		String str = "T(Integer).MAX_VALUE";
+//		String str = "T(Integer).parseInt('6666')";
+		//对象实例化
+//		String str = "new java.util.Date()";
+		//instanceof
+		String str = "'van游戏' instanceof T(String)";
 		ExpressionParser parser = new SpelExpressionParser();// 定义一个SPEL的语法解析器
 		Expression exp = parser.parseExpression(str);// 将字符串解析为一个正常的表达式对象
 		EvaluationContext context = new StandardEvaluationContext(exp);// 实现表达式的具体计算处理
-		Object value = exp.getValue(context);
-		System.out.println(value);
+//		Class<?> cls = exp.getValue(context,Class.class);
+//		Integer num = exp.getValue(context,Integer.class);
+//		Date date = exp.getValue(context,Date.class);
+		System.out.println(exp.getValue(context));
 	}
 }
