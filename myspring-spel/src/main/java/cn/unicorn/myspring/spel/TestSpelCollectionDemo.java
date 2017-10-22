87ca64a960bb6767e64406571c1da3afa2bdb3bc
Lsuka <1150859699@qq.com>
2017-10-22 15:51:57 +0800

    集合表达式(List,Set,Map)的基本操作

--- myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelCollectionDemo.java
+++ myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelCollectionDemo.java
@@ -1,5 +1,8 @@
 package cn.unicorn.myspring.spel;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
@@ -8,14 +11,24 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 public class TestSpelCollectionDemo {
 	public static void main(String[] args) throws Exception {
-		// List集合表达式,基础使用
-		String str = "{10,20,30}[1]";// 通过索引访问数据
+		// 集合表达式
+		// List<String> all = new ArrayList<>();
+		// Set<String> all = new HashSet<>();
+		// all.add("van游戏");
+		// all.add("deep♂dark");
+		// all.add("ass♂we♂can");
+		Map<String, String> all = new HashMap<>();
+		all.put("van", "deep♂dark");
+		all.put("king", "ass♂we♂can");
+		String str = "#allData['king']";// 通过索引访问数据
 		ExpressionParser parser = new SpelExpressionParser();
 		Expression exp = parser.parseExpression(str);// 设置了一个自定义的根变量
 		EvaluationContext context = new StandardEvaluationContext();
+		context.setVariable("allData", all);// 将自定义的集合设置进来
 		System.out.println(exp.getValue(context));
-		//尝试List数据的添加
-//		List<Integer> all = exp.getValue(context, List.class);
-//		all.add(50);//进行新的数据追加。不可以进行数据添加,报java.util.Collections$UnmodifiableCollection.add(Unknown Source)错误
+		// 尝试List数据的添加
+		// List<Integer> all = exp.getValue(context, List.class);
+		// all.add(50);//进行新的数据追加。不可以进行数据添加,报java.util.Collections$UnmodifiableCollection.add(Unknown
+		// Source)错误
 	}
 }
