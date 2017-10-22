274285b098f659ff1dbdff3dc94661aa190d6ae2
Lsuka <1150859699@qq.com>
2017-10-22 18:22:34 +0800

    基于Spring配置实现QuartZ的调用(实现自定义方法的调度)

--- myspring-schedule/src/main/java/cn/unicorn/myspring/schedule/quartz/MyTask.java
+++ myspring-schedule/src/main/java/cn/unicorn/myspring/schedule/quartz/MyTask.java
@@ -3,15 +3,8 @@ package cn.unicorn.myspring.schedule.quartz;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.springframework.scheduling.quartz.QuartzJobBean;
-
-public class MyTask extends QuartzJobBean {
-
-	@Override
-	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-		System.out.println("[当前的日期时间]"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
+public class MyTask {
+	protected void runTask() {// 随意定义的方法,用于被调度
+		System.out.println("[当前的日期时间]" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
 	}
-
 }
