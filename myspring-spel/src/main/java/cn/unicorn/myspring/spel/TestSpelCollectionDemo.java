cb67a995a49e147c6e7012e96483af136e09f1a0
Lsuka <1150859699@qq.com>
2017-10-22 16:01:39 +0800

    集合的投影处理,投影处理指的是可以通过表达式进行集合内容的操作

--- myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelCollectionDemo.java
+++ myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelCollectionDemo.java
@@ -11,12 +11,12 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 public class TestSpelCollectionDemo {
 	public static void main(String[] args) throws Exception {
-		//集合内容的修改
+		// 集合的投影处理,投影处理指的是可以通过表达式进行集合内容的操作
 		List<String> all = new ArrayList<>();
 		all.add("van游戏");
 		all.add("deep♂dark");
 		all.add("ass♂we♂can");
-		String str = "#allData[1]='www.bilibili.com'";//修改集合中的内容
+		String str = "#allData.!['哲♂学:'+#this]";//修改集合中的内容
 		ExpressionParser parser = new SpelExpressionParser();
 		Expression exp = parser.parseExpression(str);// 设置了一个自定义的根变量
 		EvaluationContext context = new StandardEvaluationContext();
