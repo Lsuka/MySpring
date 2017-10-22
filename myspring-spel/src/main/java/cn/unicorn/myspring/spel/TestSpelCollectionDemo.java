d51b15d36eb7b70d4583e09564aaa6493854a920
Lsuka <1150859699@qq.com>
2017-10-22 16:04:51 +0800

    (Map)集合的投影处理,投影处理指的是可以通过表达式进行集合内容的操作(刚刚那个是List)

--- myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelCollectionDemo.java
+++ myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelCollectionDemo.java
@@ -1,7 +1,9 @@
 package cn.unicorn.myspring.spel;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
@@ -12,15 +14,19 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
 public class TestSpelCollectionDemo {
 	public static void main(String[] args) throws Exception {
 		// 集合的投影处理,投影处理指的是可以通过表达式进行集合内容的操作
-		List<String> all = new ArrayList<>();
-		all.add("van游戏");
-		all.add("deep♂dark");
-		all.add("ass♂we♂can");
-		String str = "#allData.!['哲♂学:'+#this]";//修改集合中的内容
+		// List<String> all = new ArrayList<>();
+		// all.add("van游戏");
+		// all.add("deep♂dark");
+		// all.add("ass♂we♂can");
+		Map<String, String> all = new HashMap<>();
+		all.put("van", "deep♂dark");
+		all.put("king", "ass♂we♂can");
+		//整个处理之中就将Map.Entry隐含了
+		String str = "#allData.![#this.key + '-' + #this.value]";//修改集合中的内容
 		ExpressionParser parser = new SpelExpressionParser();
 		Expression exp = parser.parseExpression(str);// 设置了一个自定义的根变量
 		EvaluationContext context = new StandardEvaluationContext();
 		context.setVariable("allData", all);// 将自定义的集合设置进来
-		System.out.println(exp.getValue(context));
+		System.out.println(exp.getValue(context));//此时就是一个List集合
 	}
 }
