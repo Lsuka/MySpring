542cf3a529d7c650fa44936cd53289fb062aa3f4
Lsuka <1150859699@qq.com>
2017-10-22 15:21:07 +0800

    SPEL的方法引用操作(调用对象中的属性(getter))

--- myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelSimpleDemo.java
+++ myspring-spel/src/main/java/cn/unicorn/myspring/spel/TestSpelSimpleDemo.java
@@ -1,7 +1,8 @@
 package cn.unicorn.myspring.spel;
 
-import java.lang.reflect.Method;
+import java.util.Date;
 
+import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
@@ -9,14 +10,13 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 public class TestSpelSimpleDemo {
 	public static void main(String[] args) throws Exception {
-		// 变量操作(根变量的加入)
-		String str = "#myInt('666')";// myInt表示的就是方法引用设置的别名
-		Method method = Integer.class.getMethod("parseInt", String.class);// 获取该方法引用的反射对象
+		// SPEL的方法引用操作(调用对象中的属性(getter))
+		String str = "Time";// Date类中的getTime()方法
 		ExpressionParser parser = new SpelExpressionParser();
 		Expression exp = parser.parseExpression(str);// 设置了一个自定义的根变量
 		// 为这个根变量直接执行赋值处理操作
-		StandardEvaluationContext context = new StandardEvaluationContext();
-		context.registerFunction("myInt", method);
+		EvaluationContext context = new StandardEvaluationContext(new Date());
 		System.out.println(exp.getValue(context));
+		System.out.println(System.currentTimeMillis());
 	}
 }
