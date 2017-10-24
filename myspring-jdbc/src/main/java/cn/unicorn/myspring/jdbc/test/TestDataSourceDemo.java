505cd2417223528d5ecbb00a591a8154b7c23279
Lsuka <1150859699@qq.com>
2017-10-24 22:11:28 +0800

    JdbcTemplate的引入

--- myspring-jdbc/src/main/java/cn/unicorn/myspring/jdbc/test/TestDataSourceDemo.java
+++ myspring-jdbc/src/main/java/cn/unicorn/myspring/jdbc/test/TestDataSourceDemo.java
@@ -1,10 +1,12 @@
 package cn.unicorn.myspring.jdbc.test;
 
+import java.util.Date;
+
 import javax.annotation.Resource;
-import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -14,10 +16,30 @@ import junit.framework.TestCase;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class TestDataSourceDemo extends TestCase {
 	@Resource
-	private DataSource dataSource;
+	private JdbcTemplate jdbcTemplate;
 
 	@Test
 	public void testConnection() throws Exception {
-		System.out.println(this.dataSource.getConnection());
+		String sql = "INSERT INTO news(title,pubdate,note,price,readcount) VALUES(?,?,?,?,?)";
+		String title = "哔哩哔哩干杯";
+		Date pubdate = new Date();
+		String note = "高清无码搞比利";
+		double price = 6.6;
+		int readcount = 6;
+		int len = this.jdbcTemplate.update(sql, title, pubdate, note, price, readcount);// 更新操作方式一
+		// int len = this.jdbcTemplate.update(sql, new PreparedStatementSetter() {//
+		// 更新操作方式二
+		// //实际上这种方式给了用户自定义的空间
+		// @Override
+		// public void setValues(PreparedStatement ps) throws SQLException {
+		// ps.setString(1, title);
+		// ps.setDate(2, new java.sql.Date(pubdate.getTime()));
+		// ps.setString(3, note);
+		// ps.setDouble(4, price);
+		// ps.setInt(5, readcount);
+		// }
+		// });
+		System.out.println("更新行数" + len);
+		TestCase.assertEquals(len, 1);
 	}
 }
