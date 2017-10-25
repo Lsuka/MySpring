cd658e465fdb81199a9b1a0d3bfc739f2f9fb5d8
Lsuka <1150859699@qq.com>
2017-10-26 01:43:50 +0800

    配置资源文件

--- springmvc/src/main/java/cn/unicorn/myspring/springmvc/action/EchoAction.java
+++ springmvc/src/main/java/cn/unicorn/myspring/springmvc/action/EchoAction.java
@@ -1,22 +1,32 @@
 package cn.unicorn.myspring.springmvc.action;
 
 import java.util.Arrays;
+import java.util.Locale;
+
+import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller // 所有需要被Spring管理的程序类一定要进行配置
 @RequestMapping("/pages/message/*") // 与方法中的路径做结合
 public class EchoAction {// 定义一个自己的处理的程序类
 	private Logger log = LoggerFactory.getLogger(EchoAction.class);
+	@Resource
+	private MessageSource messageSource;
 
 	@RequestMapping("echo_pre")
 	public String echoPre() {
+		System.err.println(
+				"[echo.input.page]" + this.messageSource.getMessage("echo.input.page", null, Locale.getDefault()));
+		System.err.println("[welcome.info]"
+				+ this.messageSource.getMessage("welcome.info", new Object[] { "Mr.smith" }, Locale.getDefault()));
+		//System.err.println("[nothing]" + this.messageSource.getMessage("nothing", null, Locale.getDefault()));
 		return "message/message_input";
 	}
 
