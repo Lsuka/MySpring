7ba3ee0ecaca15def6572169ed712ec0a877b49d
Lsuka <1150859699@qq.com>
2017-10-26 00:41:04 +0800

    rest风格显示,目前就是json的展示

--- springmvc/src/main/java/cn/unicorn/myspring/springmvc/action/EmpAction.java
+++ springmvc/src/main/java/cn/unicorn/myspring/springmvc/action/EmpAction.java
@@ -1,18 +1,59 @@
 package cn.unicorn.myspring.springmvc.action;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import cn.unicorn.myspring.springmvc.util.action.AbstractAction;
+import cn.unicorn.myspring.springmvc.vo.Dept;
 import cn.unicorn.myspring.springmvc.vo.Emp;
 
 @Controller
 @RequestMapping("/pages/emp/*")
 public class EmpAction extends AbstractAction {
 	@RequestMapping("emp_add")
-	public ModelAndView add(Emp emp) {
-		System.err.println(emp);
-		return null;
+	@ResponseBody // 使用此注解就表示返回的对象自动变为JSON对象
+	public Object add(Emp emp) {
+		return emp;
+	}
+
+	@RequestMapping("emp_list")
+	@ResponseBody
+	public Object list() {
+		List<Emp> all = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			Emp vo = new Emp();
+			vo.setEmpno(7369L + i);
+			vo.setEname("Smith-" + i);
+			Dept dept = new Dept();
+			dept.setDeptno(10L + i);
+			dept.setDname("哲学部" + i);
+			vo.setDept(dept);
+			all.add(vo);
+		}
+		return all;
+	}
+
+	@RequestMapping("emp_map")
+	@ResponseBody
+	public Object map() {
+		Map<String, Emp> all = new HashMap<>();
+		for (int i = 0; i < 10; i++) {
+			Emp vo = new Emp();
+			vo.setEmpno(7369L + i);
+			vo.setEname("Smith-" + i);
+			Dept dept = new Dept();
+			dept.setDeptno(10L + i);
+			dept.setDname("哲学部" + i);
+			vo.setDept(dept);
+			all.put("雇佣信息-" + i, vo);
+		}
+		return all;
 	}
 }
